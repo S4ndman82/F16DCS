6fd9b359bf8d9aa5f740cf18f0874bb2adecb447
Ilkka Prusi <ilkka.prusi@gmail.com>
2016-09-16 17:00:52 +0300

    change to mode support

--- FlightModel/EnvironmentalSystem/F16OxygenSystem.h
+++ FlightModel/EnvironmentalSystem/F16OxygenSystem.h
@@ -16,25 +16,35 @@ class F16OxygenSystem
 protected:
 	const double tank_volume; // whole volume of tank
 	double tank_usage; // amount used
-	double diluter_setting; // normal/100% ?
+	//double diluter_setting; // normal/100% ?
 
 	double pressureGen; // pressure provided (pascals), used over ambient pressure
 
 	// sensors? (for cockpit?)
 	// valves?
 
+	enum EDiluterMode
+	{
+		DIL_OFF,
+		DIL_NORMAL,
+		DIL_RICH
+	};
+	EDiluterMode diluter;
+
 public:
 	F16OxygenSystem() 
 		: tank_volume(5.0)
 		, tank_usage(5.0)
-		, diluter_setting(0) // <- off
+		//, diluter_setting(0) // <- off
 		, pressureGen(0)
+		, diluter(DIL_OFF)
 	{}
 	~F16OxygenSystem() {}
 
 	void setDiluterNormal()
 	{
-		diluter_setting = 100;
+		diluter = DIL_NORMAL;
+		//diluter_setting = 50;
 	}
 
 	double getPressure() const
@@ -46,17 +56,21 @@ public:
 	void updateFrame(const double ambientPressure, const double altitude, const double frameTime)
 	{
 		// very rough usage
-		double use = frameTime * diluter_setting;
+		double diluter_value = 0;
+		double use = frameTime * diluter_value;
 		if (tank_usage > use)
 		{
 			tank_usage -= use;
 		}
 		//tanks += o2_gen;
 
-		// pressure provided depends on diluter setting
-		// and ambient pressure ?
-		pressureGen = 101325; // standard sea-level atmosphere
-		pressureGen -= ambientPressure; // reduce ambient pressure (result pressure over ambient)
+		if (diluter != DIL_OFF)
+		{
+			// pressure provided depends on diluter setting
+			// and ambient pressure ?
+			pressureGen = 101325; // standard sea-level atmosphere
+			pressureGen -= ambientPressure; // reduce ambient pressure (result pressure over ambient)
+		}
 	}
 };
 
